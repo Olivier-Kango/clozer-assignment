122fc0702f92f52a8c20b01c3e01c165c051225f
Olivier-Kango <olivierkango@gmail.com>
2024-06-07 17:56:41 +0300

    Add button

--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TabsData } from '../../../types/tabsData';
 import Image from 'next/image';
 import styles from '../styling/Card.module.css'; 
@@ -16,30 +16,49 @@ interface EventDetails {
 
 const Card: React.FC<CardProps> = ({ details, onClick }) => {
   const { items } = details;
+  const [startIndex, setStartIndex] = useState(0);
 
   // Filtrer les événements où Dean Martin est impliqué
   const deanMartinEvents = items.filter(item =>
     ('details' in item) && ('Artists' in item.details) && (item.details as EventDetails).Artists.includes('Dean Martin')
   );
 
+  const handleNext = () => {
+    if (startIndex < deanMartinEvents.length - 4) {
+      setStartIndex(prev => prev + 2);
+    }
+  };
+
+  const handlePrev = () => {
+    if (startIndex > 0) {
+      setStartIndex(prev => prev - 2);
+    }
+  };
+
   return (
-    <div className={styles.card} onClick={onClick}>
-       {deanMartinEvents.map(item => (
-        <div key={item.id} className={styles.cardContent}>
-          <div className={styles.cardImage}>
-            <Image
-              src={(item.details as EventDetails).CoverURL}
-              alt={(item.details as EventDetails).Title}
-              width={132}
-              height={132}
-            />
+    <div>
+      <div className={styles.container}>
+        {deanMartinEvents.slice(startIndex, startIndex + 4).map(item => (
+          <div key={item.id} className={styles.cardContent}>
+            <div className={styles.cardImage}>
+              <Image
+                src={(item.details as EventDetails).CoverURL}
+                alt={(item.details as EventDetails).Title}
+                width={132}
+                height={132}
+              />
+            </div>
+            <div className={styles.cardText}>
+              <h3>{(item.details as EventDetails).Title}</h3>
+              <p>{(item.details as EventDetails).Artists.join(', ')}</p>
+            </div>
           </div>
-          <div className={styles.cardText}>
-            <h3>{(item.details as EventDetails).Title}</h3>
-            <p>{(item.details as EventDetails).Artists.join(', ')}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className={styles['navigation-buttons']}>
+        <button onClick={handlePrev} disabled={startIndex === 0}>&lt;</button>
+        <button onClick={handleNext} disabled={startIndex >= deanMartinEvents.length - 4}>&gt;</button>
+      </div>
     </div>
   );
 };
