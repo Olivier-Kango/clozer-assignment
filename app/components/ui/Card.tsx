af0492adaaa70ab71d9a7305ef67fb9f55f3a2e1
Olivier-Kango <olivierkango@gmail.com>
2024-06-07 10:05:48 +0300

    Diplay Dean Martin Best Ever Collection

--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -8,10 +8,23 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ details, onClick }) => {
+  const item = details.items[0];
+  let Album: string | undefined;
+  let Artists: string[] | undefined;
+  let CoverURL: string | undefined;
+
+  if (item.details && 'Album' in item.details && 'Artists' in item.details && 'CoverURL' in item.details) {
+    Album = item.details.Album;
+    Artists = item.details.Artists;
+    CoverURL = item.details.CoverURL;
+  }
+
   return (
     <div className="card" onClick={onClick}>
-      <Image src={details.coverURL} alt={details.title} />
-      <h3>{details.title}</h3>
+      <h3>{Album}</h3>
+      <p>{Artists}</p>
+      {CoverURL && <Image src={CoverURL} alt={Album || "Cover"} width={200} height={200} />}
+      {/* <h3>{details.title}</h3> */}
       {/* <p>{details.artists.join(', ')}</p>
       <a href={details.links[0].url} target="_blank" rel="noopener noreferrer">
         Listen
